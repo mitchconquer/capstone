26a7dc404286a1950599ea84a996aee3aec1a192
mitchconquer <mitch@robotshanghai.com>
2016-06-29 12:00:41 -0700

    Make DB columns for links all text instead of string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629174911) do
+ActiveRecord::Schema.define(version: 20160629183006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20160629174911) do
 
   create_table "feed_items", force: :cascade do |t|
     t.integer  "feed_source_id", null: false
-    t.string   "identifier",     null: false
+    t.text     "identifier",     null: false
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
@@ -45,9 +45,9 @@ ActiveRecord::Schema.define(version: 20160629174911) do
 
   create_table "feed_sources", force: :cascade do |t|
     t.string   "title",                                                       null: false
-    t.string   "url",                                                         null: false
+    t.text     "url",                                                         null: false
     t.boolean  "recommended", default: false,                                 null: false
-    t.string   "image_url",   default: "https://placeimg.com/500/500/nature", null: false
+    t.text     "image_url",   default: "https://placeimg.com/500/500/nature", null: false
     t.datetime "created_at",                                                  null: false
     t.datetime "updated_at",                                                  null: false
   end
