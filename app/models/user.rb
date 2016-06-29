38803f3f55f3c7e9db3de96db9f856e5e26c7c23
mitchconquer <mitch@robotshanghai.com>
2016-06-29 09:41:34 -0700

    Add model validation for username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ require 'securerandom'
 class User < ActiveRecord::Base
   attr_reader :password
   validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
   after_initialize :ensure_session_token
 
