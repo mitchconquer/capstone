12b2c27d0b00a195c14b12d46a6641ab90a81838
mitchconquer <mitch@robotshanghai.com>
2016-06-29 14:30:56 -0700

    Add routes for feed_sources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [ :create ]
     resource :session, only: [ :create, :destroy ]
+
+    resources :users, only: [ :create ]
+
+    resources :feed_sources, path: 'feeds' do
+      resources :feed_items, path: 'items', only: [ :index ]
+    end
   end 
 end
