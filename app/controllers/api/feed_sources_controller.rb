31234f1510f91e40b7b533816c36df1cbac6bcef
mitchconquer <mitch@robotshanghai.com>
2016-06-30 11:24:04 -0700

    Start of skeleton for adding new feeds

--- app/controllers/api/feed_sources_controller.rb
+++ app/controllers/api/feed_sources_controller.rb
@@ -1,4 +1,11 @@
 class Api::FeedSourcesController < ApplicationController
+
+  def create
+    @feed_source = FeedSource.new({url: "Test URL from controller", title: "Test Title from controller"})
+
+    render :show
+  end
+
   def show
     @feed_source = FeedSource.includes(:feed_items).find(params[:id])
     @read_feed_items = @feed_source.read_feed_items(current_user)
