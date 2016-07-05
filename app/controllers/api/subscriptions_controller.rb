45910019807b74f5e9307c617be6ea14f028e097
mitchconquer <mitch@robotshanghai.com>
2016-07-04 21:49:28 -0700

    Fix subscription controller so deletes subscriptions on correct API call

--- app/controllers/api/subscriptions_controller.rb
+++ app/controllers/api/subscriptions_controller.rb
@@ -7,14 +7,10 @@ class Api::SubscriptionsController < ApplicationController
   end
 
   def destroy
-    @subscription = Subscription.where(folder_id: params[:folder_id], feed_source_id: params[:feed_source_id], user_id: current_user.id)
+    @subscription = Subscription.where(folder_id: params[:folder_id], feed_source_id: params[:feed_source_id], user_id: current_user.id).first
 
-    unless @subscription.empty?
-      feed_source_ids = []
-      @subscription.first do |sub|
-        feed_source_ids.push sub.feed_source_id
-        Subscription.destroy(sub)
-      end
+    if @subscription
+      @subscription.destroy
 
       @folder = Folder.includes(:feed_sources).find(params[:folder_id])
     
