e44b28d7b935355f328cdef0e770537cbf7b2ded
mitchconquer <mitch@robotshanghai.com>
2016-07-07 16:12:23 -0700

    Verified read item API

--- app/controllers/api/read_feed_records_controller.rb
+++ app/controllers/api/read_feed_records_controller.rb
@@ -5,13 +5,13 @@ class Api::ReadFeedRecordsController < ApplicationController
   end
 
   def create
-    @read_feed_record = curent_user.read_feed_records.create(feed_item_id: params[:id])
+    @read_feed_record = current_user.read_feed_records.create(feed_item_id: params[:id])
     render :show
   end
 
   def destroy
     @read_feed_record = ReadFeedRecord.find_by(user_id: current_user.id, feed_item_id: params[:id])
     @read_feed_record.destroy
-    render :json, @read_feed_record.id
+    render :show
   end
 end
