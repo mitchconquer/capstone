0cdabba0cf133660823c7396bae7af60b411b1e5
mitchconquer <mitch@robotshanghai.com>
2016-07-06 23:47:40 -0700

    Added clear filter button to folder index utils filter bar

--- frontend/components/folder_index_utils.jsx
+++ frontend/components/folder_index_utils.jsx
@@ -15,15 +15,20 @@ const FolderIndexUtils = React.createClass({
     FeedActions.filter(this.state.filterText);
   },
 
+  clearSearch() {
+    this.setState({ filterText: "" }, this.filterFeeds);
+  },
+
   filterTextChange(e) {
     this.setState({ filterText: e.target.value }, this.filterFeeds);
   },
 
   render(){
+    const icon = this.state.filterText.length > 0 ? "remove" : "search"
     return (
       <form className="filter-feeds-form">
         <ControlLabel srOnly={true} htmlFor="folder-filter">Filter</ControlLabel>
-        <span className="glyphicon glyphicon-search"></span>
+        <span className={"glyphicon glyphicon-" + icon} onClick={this.clearSearch}></span>
         <FormControl type="text" placeholder="Filter feeds..." id="folder-filter" className="feed-groups-filter transparent" value={this.state.filterText} onChange={this.filterTextChange} />
       </form>
     );
