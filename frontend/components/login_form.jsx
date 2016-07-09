3821d55c9909fe675fc12231d8c5f5da7ae65448
mitchconquer <mitch@robotshanghai.com>
2016-07-09 08:47:23 -0700

    Added logging in state to login form that shows btn-spinner

--- frontend/components/login_form.jsx
+++ frontend/components/login_form.jsx
@@ -10,17 +10,46 @@ const LoginForm = React.createClass({
 
   getInitialState() {
       return {
-          username: "demo", password: "demouser", errors: {} 
+          username: "", password: "", errors: {}, loggingIn: false
       };
   },
 
   onFormSubmit(e) {
-    e.preventDefault();
+    if (e) {
+      e.preventDefault();
+    }
+    this.submitForm();
+  },
+
+  submitForm() {
+    this.setState({ loggingIn: true })
     const formData = { user: 
      { username: this.state.username,
        password: this.state.password } }
 
     SessionActions.login(formData);
+  },
+
+  demoLogin() {
+    this.setState({ username: "", password: "", loggingIn: true });
+
+    const username = "demo";
+    const password = "demouser";
+    const baseTime = 100;
+
+    username.split('').forEach((letter, index) => {
+      window.setTimeout(() => {
+        this.setState({ username: this.state.username + letter });
+      }, baseTime * (index + 1));
+    });
+
+    password.split('').forEach((letter, index) => {
+      window.setTimeout(() => {
+        this.setState({ password: this.state.password + letter });
+      }, baseTime * (index + 1 + username.length));
+    });
+
+    window.setTimeout(this.submitForm, baseTime * (username.length + password.length + 3) );
   },
 
   componentDidMount() {
@@ -29,7 +58,7 @@ const LoginForm = React.createClass({
   },
 
   errorChange() {
-    this.setState({ errors: ErrorStore.formErrors('LoginForm') });
+    this.setState({ errors: ErrorStore.formErrors('LoginForm'), loggingIn: false });
   },
 
   redirectIfLoggedIn() {
@@ -51,15 +80,25 @@ const LoginForm = React.createClass({
           return (<li key={key}>{this.state.errors[key]}</li>);
         });
 
+    const defaultSubmit = <input type="submit" value="Login" className="form-control btn btn-success" />;
+    const loadingSubmit = (<a className="form-control btn btn-success"><div className="btn-spinner-text">Logging in...</div><div className="spinner"><div className="double-bounce1"></div> <div className="double-bounce2"></div> </div></a>);
+
+    let submitBtn;
+    if (this.state.loggingIn) {
+      submitBtn = loadingSubmit;
+    } else {
+      submitBtn = defaultSubmit;
+    }
+
     return(
       <section className="login-signup-container">
         <div className="login-signup-form">
           <h1 className="site-title">AggreGreater &gt;</h1>
           <h3 className="site-description">A simple RSS reader and news aggregator.</h3>
-          <UserAccountLink />
-          <div>
+          <div className="errors">
             {errors}
           </div>
+          <UserAccountLink />
           <form className="login-form" onSubmit={this.onFormSubmit}>
             <label for="username">Username:</label><br />
             <input type="text" onChange={this.usernameChange} value={this.state.username} id="username" className="form-control transparent login-form" />
@@ -67,11 +106,13 @@ const LoginForm = React.createClass({
             <label for="password">Password:</label><br />
             <input type="password" onChange={this.passwordChange} value={this.state.password} id="password" className="form-control transparent login-form" />
             <br />
-            <input type="submit" value="Login" className="form-control btn btn-success" />
+            {submitBtn}
+            <input type="button" onClick={this.demoLogin} value="Demo Login" className="form-control btn btn-success" />
+            
           </form>
         </div>
       </section>
-  );
+    );
   }
 });
 
