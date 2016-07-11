a8c8fdefd82d6a5f62484e656c3083e3741e473a
mitchconquer <mitch@robotshanghai.com>
2016-07-10 18:29:24 -0700

    Fix for mark all read/unread

--- app/controllers/api/read_feed_records_controller.rb
+++ app/controllers/api/read_feed_records_controller.rb
@@ -14,7 +14,7 @@ class Api::ReadFeedRecordsController < ApplicationController
     @read_feed_records = []
 
     feed_item_ids.each do |feed_item_id|
-      @read_feed_records.push current_user.read_feed_records.create(feed_item_id: feed_item_id)
+      @read_feed_records.push ReadFeedRecord.find_or_create_by(user_id: current_user.id, feed_item_id: feed_item_id)
     end
     render :index
   end
--- app/views/api/read_feed_records/index.json.jbuilder
+++ app/views/api/read_feed_records/index.json.jbuilder
@@ -1,3 +1,3 @@
 @read_feed_records.each do |read_feed_record|
-  json.set! :readFeedRecordId, read_feed_record.id
+  json.set! read_feed_record.feed_item_id, read_feed_record.id
 end
