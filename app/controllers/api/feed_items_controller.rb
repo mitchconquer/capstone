7a3b31a0ee7cf27aa7810f3bb38584e63e925a1d
mitchconquer <mitch@robotshanghai.com>
2016-07-11 19:29:31 -0700

    Fix sorting of feed items coming from rails controller

--- app/controllers/api/feed_items_controller.rb
+++ app/controllers/api/feed_items_controller.rb
@@ -9,6 +9,7 @@ class Api::FeedItemsController < ApplicationController
     FeedSource.find(params[:feed_source_id]).refresh
     feed_sources = params[:feed_sources]
     @feed_items = FeedItem.where("feed_source_id IN (?)", feed_sources).order(pub_date: :desc).limit(20)
+    @feed_items = @feed_items.sort_by{ |item| item.pub_date.to_i }.reverse
     render :index
   end
 end
